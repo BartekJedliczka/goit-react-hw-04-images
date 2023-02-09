c9b5454ea4044f3e25aecdeefc264a41d293b724
BartekJedliczka <bartekjedliczka@gmail.com>
2023-02-08 19:42:15 -0500

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import css from './Searchbar.module.css';
 import PropTypes from 'prop-types';
 
-class Searchbar extends Component {
-  render() {
-    const { query, onSubmit, onChange } = this.props;
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={onSubmit}>
-          <button type="submit" className={css.SearchForm__button}>
-            <span className={css.SearchForm__button__label}>Search</span>
-          </button>
-          <input
-            onChange={onChange}
-            className={css.SearchForm__input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={query}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+const Searchbar = ({ query, onSubmit, onChange }) => {
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={onSubmit}>
+        <button type="submit" className={css.SearchForm__button}>
+          <span className={css.SearchForm__button__label}>Search</span>
+        </button>
+        <input
+          onChange={onChange}
+          className={css.SearchForm__input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={query}
+        />
+      </form>
+    </header>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
