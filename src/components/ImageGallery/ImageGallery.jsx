727ba86e6d9394765799a635d632e981f8e2c9c9
BartekJedliczka <bartekjedliczka@gmail.com>
2023-02-09 09:20:54 -0500

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,38 +1,36 @@
-import { Component } from "react";
-import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
+import React from 'react';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-import css from "./ImageGallery.module.css";
+import css from './ImageGallery.module.css';
 
-export default class ImageGallery extends Component {
-    render() {
-        const { images, onShow } = this.props;
-
-        return (
-            <ul className={css.ImageGallery}>
-                {images.map(({ webformatURL, largeImageURL, id, tags }) => (
-                    <ImageGalleryItem
-                        key={id}
-                        webformatURL={webformatURL}
-                        largeImageURL={largeImageURL}
-                        onShow={onShow}
-                        tags={tags}
-                    />
-                ))}
-            </ul>
-        );
-    }
-}
+const ImageGallery = ({ images, onShow }) => {
+  return (
+    <ul className={css.ImageGallery}>
+      {images.map(({ webformatURL, largeImageURL, id, tags }) => (
+        <ImageGalleryItem
+          key={id}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          onShow={onShow}
+          tags={tags}
+        />
+      ))}
+    </ul>
+  );
+};
 
 ImageGallery.propTypes = {
-    onShow: PropTypes.func,
-    images: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            largeImageURL: PropTypes.string.isRequired,
-            webformatURL: PropTypes.string.isRequired,
-            tags: PropTypes.string.isRequired,
-        })
-    ),
+  onShow: PropTypes.func,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+    })
+  ),
 };
+
+export default ImageGallery;
