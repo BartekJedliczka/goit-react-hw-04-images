56156fbe2bcc8ec7d100b59eb71bfc2a3182b154
BartekJedliczka <bartekjedliczka@gmail.com>
2023-02-09 09:27:25 -0500

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,30 +1,26 @@
-import React, { Component } from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import PropTypes from "prop-types";
+import css from './ImageGalleryItem.module.css';
 
-import css from "./ImageGalleryItem.module.css";
-
-class ImageGalleryItem extends Component {
-    render() {
-        const { webformatURL, largeImageURL, onShow, tags } = this.props;
-        return (
-            <li className={css.galleryItem}>
-                <img
-                    className={css.ImageGalleryItemImage}
-                    onClick={() => onShow(largeImageURL)}
-                    src={webformatURL}
-                    alt={tags}
-                />
-            </li>
-        );
-    }
-}
+const ImageGalleryItem = ({ webformatURL, largeImageURL, onShow, tags }) => {
+  return (
+    <li className={css.galleryItem}>
+      <img
+        className={css.ImageGalleryItemImage}
+        onClick={() => onShow(largeImageURL)}
+        src={webformatURL}
+        alt={tags}
+      />
+    </li>
+  );
+};
 
 ImageGalleryItem.propTypes = {
-    onShow: PropTypes.func,
-    webformatURL: PropTypes.string.isRequired,
-    largeImageURL: PropTypes.string.isRequired,
-    tags: PropTypes.string.isRequired,
+  onShow: PropTypes.func,
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
 };
 
 export default ImageGalleryItem;
